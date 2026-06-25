dbc5dc25101b7f9e735f937f19aeb557b59a65c4
Francisco Blanco <blancofrancisco34@gmail.com>
2026-06-24 22:05:28 -0500

    Release 1.1.6

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Connexion Drive Excel Product
 Plugin URI: https://github.com/franciscoblancojn/connexion-drive-exel-product
 Description: Connect to Google Drive, select Excel files, map columns (SKU, price, quantity), and mass update WooCommerce products.
-Version: 1.1.5
+Version: 1.1.6
 Author: franciscoblancojn
 Author URI: https://franciscoblanco.vercel.app/
 License: GPL2+
